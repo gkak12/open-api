84d8b096ec294ec05e948a764c8ca0cefb74761b
hyunjun <hyunjun5681@kakao.com>
2025-03-04 22:14:10 +0900

    메인 어플리케이션 코드 수정

--- src/main/java/com/open/api/OpenApiApplication.java
+++ src/main/java/com/open/api/OpenApiApplication.java
@@ -1,7 +1,11 @@
 package com.open.api;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class OpenApiApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(OpenApiApplication.class, args);
     }
 }
